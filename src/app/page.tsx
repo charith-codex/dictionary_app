931fc2cc972458c1aedc351dcd0d03a5805527e8
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-07-02 09:58:58 +0530

    version 1.1
    style input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <main>
       <Banner />
+
+      <section className="absolute top-[27vh] bg-white w-[80%] ml-[10%] shadow-2xl p-5 rounded-2xl">
+        <span>hello</span>
+      </section>
     </main>
   );
 }
