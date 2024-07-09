b1844264c8f81da9cefe71d44036bbee44ba4d7f
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-07-09 13:04:00 +0530

    version 1.8
    fix some bugs
    remove some sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,6 @@ import Player from "@madzadev/audio-player";
 import "@madzadev/audio-player/dist/index.css";
 import { useEffect, useState } from "react";
 
-const tracks = [
-  {
-    url: "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3",
-    title: "Hello",
-    tags: ["hello"],
-  },
-];
-
 const colors = {
   tagsBackground: "#3e32e4",
   tagsText: "#ffffff",
@@ -42,10 +34,10 @@ export default function Home() {
 
   useEffect(() => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-      .then(async res => {
+      .then(async (res) => {
         const data = await res.json();
-        setDictionary(data);
-        console.log(data);
+        setDictionary(data[0]);
+        console.log(data[0]);
       })
       .catch((err) => {
         console.log(err);
@@ -69,60 +61,44 @@ export default function Home() {
           </h1>
           <span className="shadow-md px-6 py-2 rounded-lg bg-blue-600 text-white">
             <span className="h-[10px] w-[10px] bg-blue-300 rounded-full inline-block"></span>{" "}
-            {dictionary && dictionary[0]?.phonetics && dictionary[0].phonetics[1]?.text &&
-              `phonetic: ${dictionary[0].phonetics[1].text}`}
+            {dictionary &&
+              dictionary.phonetics &&
+              dictionary.phonetics[1]?.text &&
+              `phonetic: ${dictionary.phonetics[1].text}`}
           </span>
         </div>
 
-        <section className="mt-5 pt-4 border-solid border-0 border-t border-gray-300">
-          <span className="shadow-md px-6 py-2 rounded-lg font-semibold bg-white text-green-700 flex items-center justify-between max-w-[120px]">
-            <span className="h-[10px] w-[10px] bg-yellow-300 rounded-full inline-block"></span>
-            Origin
-          </span>
-          <p className="py-3 text-gray-600 bg-gray-100 mt-4 px-4 rounded-lg">
-            "early 19th century: variant of earlier hollo ; related to holla."
-          </p>
-        </section>
-
-        {dictionary && dictionary[0]?.meanings?.map((data: any, index: number) => (
-          <section
-            key={index}
-            className="mt-8 shadow-2xl p-5 rounded-xl bg-gray-100"
-          >
-            <span className="shadow-md px-8 py-2 rounded-lg font-semibold bg-gray-600 text-white flex items-center justify-between max-w-[150px]">
-              <span className="h-[10px] w-[10px] bg-white rounded-full inline-block"></span>
-              Meaning
-            </span>
-
-            <div>
-              <span className="shadow-md mt-4 px-3 py-2 rounded-full font-semibold bg-white text-green-700 flex items-center justify-between max-w-[330px]">
-                <span className="h-[10px] w-[10px] bg-yellow-300 rounded-full inline-block"></span>
-                Part of speech
-                <span className="shadow-md px-6 py-2 rounded-full font-semibold bg-green-600 text-white flex items-center justify-between max-w-[400px]">
-                  Exclamation
-                </span>
+        {dictionary &&
+          dictionary.meanings?.map((data: any, index: number) => (
+            <section
+              key={index}
+              className="mt-8 shadow-2xl p-5 rounded-xl bg-gray-100"
+            >
+              <span className="shadow-md px-8 py-2 rounded-lg font-semibold bg-gray-600 text-white flex items-center justify-between max-w-[150px]">
+                <span className="h-[10px] w-[10px] bg-white rounded-full inline-block"></span>
+                Meaning
               </span>
-              <p className="py-3 text-gray-600 bg-white mt-4 px-4 rounded-lg">
-                "used as a greeting or to begin a phone conversation."
-              </p>
-              <p className="py-3 text-gray-600 bg-white mt-4 px-4 rounded-lg">
-                "hello there, Katie!"
-              </p>
-            </div>
-          </section>
-        ))}
 
-        <div className="mt-4">
-          <Player
-            trackList={tracks}
-            includeTags={false}
-            includeSearch={false}
-            showPlaylist={false}
-            sortTracks={false}
-            autoPlayNextTrack={false}
-            customColorScheme={colors}
-          />
-        </div>
+              <div>
+                <span className="shadow-md mt-4 px-3 py-2 rounded-full font-semibold bg-white text-green-700 flex items-center justify-between max-w-[330px]">
+                  <span className="h-[10px] w-[10px] bg-yellow-300 rounded-full inline-block"></span>
+                  Part of speech
+                  <span className="shadow-md px-6 py-2 rounded-full font-semibold bg-green-600 text-white flex items-center justify-between max-w-[400px]">
+                    {data.partOfSpeech}
+                  </span>
+                </span>
+                {data.definitions.map((e: any, i: any) => (
+                  <p
+                    key={i}
+                    className="py-3 text-gray-600 bg-white mt-4 px-4 rounded-lg"
+                  >
+                    {e.definition}
+                  </p>
+                ))}
+              </div>
+            </section>
+          ))}
+
       </section>
     </main>
   );
